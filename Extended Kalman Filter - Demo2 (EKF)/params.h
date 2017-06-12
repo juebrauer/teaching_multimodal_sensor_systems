46799ea682b6103f36eb5082463b29addf5d7ea2
juebrauer <juergen.brauer@hs-kempten.de>
2017-06-12 22:44:19 +0200

    Adding solution for EKF exercise.

---
+++ Extended Kalman Filter - Demo2 (EKF)/params.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define IMG_WIDTH  800
+#define IMG_HEIGHT 800
+
+
+// if set to true, naive estimated state will be shown
+#define SHOW_NAIVE_ESTIMATION true
+
+// if set to true, EKF estimated state will be shown
+#define SHOW_EKF_ESTIMATION true
+
+// if set to true, EKF debug information will be displayed
+// on the console
+#define SHOW_DEBUG_INFO false
+
+
+#define COL_GT             CV_RGB(255, 255, 255)
+#define COL_NAIVE          CV_RGB(  0, 255,   0)
+#define COL_MEASUREMENT    CV_RGB(  0, 255, 255)
+#define COL_EKF            CV_RGB(255,   0,   0)
+
+
+// how many last points of the trajectory shall be
+// saved and displayed?
+#define MAX_NR_TRAJECTORY_POINTS 200
